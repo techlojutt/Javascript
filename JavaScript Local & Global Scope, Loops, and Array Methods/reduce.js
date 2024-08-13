5b8230024b373776c56f07f52ef0f51b566e09b3
techlojutt <muhammadshoaib2140jutt@gmail.com>
2024-08-13 19:27:30 +0500

    adding reduce loop

--- JavaScript Local & Global Scope, Loops, and Array Methods/reduce.js
+++ JavaScript Local & Global Scope, Loops, and Array Methods/reduce.js
@@ -1,8 +1,24 @@
+// reduce() is a loop which returns the last return on every iteration 
 var listOfNumbers = [10,20,30,40,50,60];
+var namelist = ['Shoaib',"bilal","Saad","Umair"]
 
-function reduceCallBack(lastReturn ,currentReturn,index,array){
-    
+function reduceCallBack(lastReturn,currentitem,index,array){
+
+ console.log("current item",currentitem);
+ console.log("last return",lastReturn);
+ 
+ 
+  return currentitem+lastReturn;
+ 
 }
+
+var result = listOfNumbers.reduce(reduceCallBack);
+console.log(result);
+
+
+
+
+
 
 
 
